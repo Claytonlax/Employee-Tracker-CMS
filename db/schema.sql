f49419a86f1c266ebbd26b8932348cb35cfa9005
Claytonlax <130805846+Claytonlax@users.noreply.github.com>
2023-09-06 14:47:23 -0700

    Trimmed unnecesary files

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,26 @@
-DROP DATABASE IF EXISTS user_db;
+drop database if exists employees_db;
 
-CREATE DATABASE user_db;
+create database employees_db;
+
+use employees_db;
+
+create table employees(
+    id int not null auto_increment,
+    first_name varchar(20) not null, 
+    last_name varchar(20) not null,
+    title varchar(20),
+    department varchar(40),
+    salary int,
+    manager varchar(40),
+    primary key (id)
+
+)
+
+create table roles(
+    id int not null auto_increment,
+    title varchar(40),
+    department varchar(40),
+    salary int, 
+    foreign key (employee_id) references employees(id),
+    primary key (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+insert into employees(id, first_name, last_name, title, department, salary, manager) values 
+(1, "Bob", "Smith", "Sales Assosciate", "Sales",100000,"Steve Robinson")
